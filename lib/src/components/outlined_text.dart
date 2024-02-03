fd5c4a3af783aa20b96a33b14f841465fc5161e3
fakhri <fakhri.fironus@tmrnd.com.my>
2024-02-04 07:17:05 +0800

    🔷 create outlinedText component

---
+++ lib/src/components/outlined_text.dart
@@ -0,0 +1,46 @@
+import 'dart:ui';
+
+import 'package:flame/components.dart';
+import 'package:flame/palette.dart';
+import 'package:flame/text.dart';
+
+class OutlinedText extends TextComponent {
+  final double fontSize;
+  final double thickness;
+  final Color? textColor;
+  final Color? outlineColor;
+
+  OutlinedText(
+      {this.fontSize = 50,
+      this.thickness = 1.5,
+      this.textColor,
+      this.outlineColor,
+      required super.text,
+      super.position,
+      super.size,
+      super.scale,
+      super.anchor,
+      super.priority,
+      super.children})
+      : super(
+            textRenderer: TextPaint(
+          style: TextStyle(
+              fontSize: fontSize,
+              shadows: [
+                Shadow(
+                    offset: Offset(-thickness, -thickness),
+                    color: outlineColor ?? BasicPalette.black.color),
+                Shadow(
+                    offset: Offset(thickness, -thickness),
+                    color: outlineColor ?? BasicPalette.black.color),
+                Shadow(
+                    offset: Offset(thickness, thickness),
+                    color: outlineColor ?? BasicPalette.black.color),
+                Shadow(
+                    offset: Offset(-thickness, thickness),
+                    color: outlineColor ?? BasicPalette.black.color),
+              ],
+              color: textColor ?? BasicPalette.white.color,
+              fontFamily: 'Knewave'),
+        ));
+}

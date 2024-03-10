f7b388115d5ae5b8a85af3d5b2a2b7fbaffe2d30
fakhri <fakhri.fironus@tmrnd.com.my>
2024-03-11 02:47:20 +0800

    🌐 update translation for powerups

--- lib/src/overlays/powerup.dart
+++ lib/src/overlays/powerup.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flame/palette.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -18,75 +19,78 @@ class PowerupOverlay extends StatefulWidget {
 
 class _PowerupOverlayState extends State<PowerupOverlay> {
   @override
-  Widget build(BuildContext context) =>
-      TemplateOverlay(game: widget.game, title: 'Powerup', children: [
-        TextFormField(
-          onChanged: (value) {
-            if (value.length != 8) return;
+  Widget build(BuildContext context) => TemplateOverlay(
+          game: widget.game,
+          title: 'powerup.header'.tr(),
+          children: [
+            TextFormField(
+              onChanged: (value) {
+                if (value.length != 8) return;
 
-            final index = value.indexOf('X');
+                final index = value.indexOf('X');
 
-            if (index.isNegative || index > 5) return;
+                if (index.isNegative || index > 5) return;
 
-            final isValid = value
-                    .replaceAll('X', '')
-                    .split('')
-                    .takeWhile((value) => Powerup.values[index].name
-                        .toUpperCase()
-                        .contains(value))
-                    .length ==
-                7;
+                final isValid = value
+                        .replaceAll('X', '')
+                        .split('')
+                        .takeWhile((value) => Powerup.values[index].name
+                            .toUpperCase()
+                            .contains(value))
+                        .length ==
+                    7;
 
-            if (isValid) {
-              widget.game.overlays.clear();
-              HapticFeedback.heavyImpact();
-              scaffoldMessengerKey.currentState?.showSnackBar(SnackBar(
-                elevation: 0,
-                backgroundColor: Colors.transparent,
-                duration: const Duration(seconds: 2),
-                dismissDirection: DismissDirection.none,
-                content: Chip(
-                    label: Text(Powerup.values[index].message,
-                        style: Theme.of(context).primaryTextTheme.bodyMedium),
-                    backgroundColor: Colors.black,
-                    shape: const StadiumBorder(),
-                    side: const BorderSide(color: Colors.white12)),
-              ));
+                if (isValid) {
+                  widget.game.overlays.clear();
+                  HapticFeedback.heavyImpact();
+                  scaffoldMessengerKey.currentState?.showSnackBar(SnackBar(
+                    elevation: 0,
+                    backgroundColor: Colors.transparent,
+                    duration: const Duration(seconds: 2),
+                    dismissDirection: DismissDirection.none,
+                    content: Chip(
+                        label: Text(Powerup.values[index].message,
+                            style:
+                                Theme.of(context).primaryTextTheme.bodyMedium),
+                        backgroundColor: Colors.black,
+                        shape: const StadiumBorder(),
+                        side: const BorderSide(color: Colors.white12)),
+                  ));
 
-              switch (index) {
-                case 0:
-                  widget.game.bladeColor = BasicPalette.red.color;
-                  break;
-                case 1:
-                  widget.game.bladeColor = BasicPalette.orange.color;
-                  break;
-                case 2:
-                  widget.game.canCollectRocks = true;
-                  break;
-                case 3:
-                  widget.game.maxMistake = 4;
-                  break;
-                case 4:
-                  widget.game.backgroundImage = 'beach';
-                  break;
-                case 5:
-                  widget.game.backgroundImage = 'cove';
-                  break;
-                default:
-              }
-            }
-          },
-          textCapitalization: TextCapitalization.characters,
-          style: Theme.of(context).textTheme.bodyMedium,
-          maxLength: 8,
-          decoration: const InputDecoration(
-              isDense: true,
-              fillColor: Colors.white,
-              filled: true,
-              enabledBorder:
-                  OutlineInputBorder(borderSide: BorderSide(width: 2)),
-              border: OutlineInputBorder(),
-              hintText: 'Enter code to unlock powerups'),
-        ),
-      ]);
+                  switch (index) {
+                    case 0:
+                      widget.game.bladeColor = BasicPalette.red.color;
+                      break;
+                    case 1:
+                      widget.game.bladeColor = BasicPalette.orange.color;
+                      break;
+                    case 2:
+                      widget.game.canCollectRocks = true;
+                      break;
+                    case 3:
+                      widget.game.maxMistake = 4;
+                      break;
+                    case 4:
+                      widget.game.backgroundImage = 'beach';
+                      break;
+                    case 5:
+                      widget.game.backgroundImage = 'cove';
+                      break;
+                    default:
+                  }
+                }
+              },
+              textCapitalization: TextCapitalization.characters,
+              style: Theme.of(context).textTheme.bodyMedium,
+              maxLength: 8,
+              decoration: InputDecoration(
+                  isDense: true,
+                  fillColor: Colors.white,
+                  filled: true,
+                  enabledBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(width: 2)),
+                  border: const OutlineInputBorder(),
+                  hintText: 'powerup.hintText'.tr()),
+            ),
+          ]);
 }
